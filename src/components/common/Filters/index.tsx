73dd2a71a9a83e07ee40636a574167acec49cb38
Meghna Srivastava <meghna.srivastava@auto1.com>
2019-04-22 15:58:11 +0200

    modified the min and max values for slider

--- src/components/common/Filters/index.tsx
+++ src/components/common/Filters/index.tsx
@@ -16,9 +16,9 @@ export function Filters(props: FiltersProps) {
       <Radio label="Photo" options={options} />
       <Radio label="In Contact" options={options} />
       <Radio label="Favorite" options={options} />
-      <RangePicker label="Compatibility Score" range={true} defaultValue={[1, 99]}  unit='%' />
-      <RangePicker label="Age" range={true} defaultValue={[18, 95]}  unit='y/o' />
-      <RangePicker label="Height" min={0} max={300} range={true} defaultValue={[135, 210]}  unit='cm' />
+      <RangePicker label="Compatibility Score" min={1} max={99} range={true} defaultValue={[1, 99]}  unit='%' />
+      <RangePicker label="Age" min={18} range={true} defaultValue={[18, 95]}  unit='y/o' />
+      <RangePicker label="Height" min={135} max={300} range={true} defaultValue={[135, 210]}  unit='cm' />
       <RangePicker label="Distance" min={0} max={400} range={false} defaultValue={distanceValue} unit='km' /> 
     </div>
   );
